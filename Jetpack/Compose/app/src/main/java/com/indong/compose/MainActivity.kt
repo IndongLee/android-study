b6bf1279701e8d891e6595c0f22eb4c86b933d2e
IndongLee <kundera@kakao.com>
2021-01-17 03:04:03 +0900

    Material Theme

--- Jetpack/Compose/app/src/main/java/com/indong/compose/MainActivity.kt
+++ Jetpack/Compose/app/src/main/java/com/indong/compose/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -25,19 +29,40 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun NewStory() {
     val image = imageResource(id = R.drawable.header)
-    Column(Modifier.padding(16.dp)) {
-        val imageModifier = Modifier
-            .preferredHeight(180.dp)
-            .fillMaxHeight()
-        Image(image,
-        modifier = imageModifier,
-        contentScale = ContentScale.Crop)
+    MaterialTheme {
+        val typography = MaterialTheme.typography
 
-        Spacer(modifier = Modifier.preferredHeight(16.dp))
+        Column(Modifier.padding(16.dp)) {
+            val imageModifier = Modifier
+                .preferredHeight(180.dp)
+                .fillMaxHeight()
+                .clip(RoundedCornerShape(4.dp))
 
-        Text(text = "A day in Shark Fin Cove")
-        Text(text = "Davenport, California")
-        Text(text = "December 2018")
+            Image(
+                image,
+                modifier = imageModifier,
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.preferredHeight(16.dp))
+
+            Text(
+                text = "A day wandering through the sandhills " +
+                        "in Shark Fin Cove, and a few of the " +
+                        "sights I saw",
+                style = typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = "Davenport, California",
+                style = typography.body2
+            )
+            Text(
+                text = "December 2018",
+                style = typography.body2
+            )
+        }
     }
 }
 
