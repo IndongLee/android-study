f1bf1e8702b2c9fae5a0c5c99812fb1018d360b0
IndongLee <kundera@kakao.com>
2021-01-17 02:46:36 +0900

    이미지

--- Jetpack/Compose/app/src/main/java/com/indong/compose/MainActivity.kt
+++ Jetpack/Compose/app/src/main/java/com/indong/compose/MainActivity.kt
@@ -2,11 +2,16 @@ package com.indong.compose
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +24,17 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun NewStory() {
-    Column {
+    val image = imageResource(id = R.drawable.header)
+    Column(Modifier.padding(16.dp)) {
+        val imageModifier = Modifier
+            .preferredHeight(180.dp)
+            .fillMaxHeight()
+        Image(image,
+        modifier = imageModifier,
+        contentScale = ContentScale.Crop)
+
+        Spacer(modifier = Modifier.preferredHeight(16.dp))
+
         Text(text = "A day in Shark Fin Cove")
         Text(text = "Davenport, California")
         Text(text = "December 2018")
