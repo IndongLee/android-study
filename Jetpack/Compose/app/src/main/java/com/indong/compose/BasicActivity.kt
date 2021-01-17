e02f87c16eee87d220b3233b21b58a87b0f6ef5d
IndongLee <kundera@kakao.com>
2021-01-18 01:45:06 +0900

    State in Compose

--- Jetpack/Compose/app/src/main/java/com/indong/compose/BasicActivity.kt
+++ Jetpack/Compose/app/src/main/java/com/indong/compose/BasicActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
@@ -34,6 +37,8 @@ fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
                     Greeting(name = name)
                     Divider(color = Color.Black)
                 }
+                Divider(color = Color.Transparent, thickness = 32.dp)
+                Counter()
             }
         }
     }
@@ -43,6 +48,15 @@ fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
 fun Greeting(name: String) {
     Text(text = "Hello $name",
         modifier = Modifier.padding(24.dp))
+}
+
+@Composable
+fun Counter() {
+    val count = remember { mutableStateOf(0) }
+    
+    Button(onClick = { count.value++ }) {
+        Text(text = "I've been clicked ${count.value} times")
+    }
 }
 
 @Preview(showBackground = true, name = "Text Preview")
