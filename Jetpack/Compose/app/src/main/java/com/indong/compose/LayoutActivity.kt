42b22cd1eabd9b6cdae5ebe34f4a1edaa1fafff6
IndongLee <kundera@kakao.com>
2021-01-28 01:50:11 +0900

    Intrinsics

--- Jetpack/Compose/app/src/main/java/com/indong/compose/LayoutActivity.kt
+++ Jetpack/Compose/app/src/main/java/com/indong/compose/LayoutActivity.kt
@@ -335,6 +335,43 @@ private fun decoupledConstraints(margin: Dp): ConstraintSet {
     }
 }
 
+@ExperimentalLayout
+@Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    Row(modifier = modifier.preferredHeight(IntrinsicSize.Min)) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start),
+            text = text1
+        )
+
+        Divider(color = Color.Black, modifier = Modifier
+            .fillMaxHeight()
+            .preferredWidth(1.dp))
+        Text(
+            modifier = Modifier
+                .weight(1f)
+
+                .padding(end = 4.dp)
+                .wrapContentWidth(Alignment.End),
+            text = text2
+        )
+    }
+}
+
+@ExperimentalLayout
+@Preview
+@Composable
+fun TwoTextsPreview() {
+    ComposeTheme {
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "there")
+        }
+    }
+}
+
 @Preview
 @Composable
 fun LargeConstraintLayoutPreview() {
