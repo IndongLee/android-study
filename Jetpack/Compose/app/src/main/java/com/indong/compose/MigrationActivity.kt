40ead4ce9d1a58389e68736d0e7a680450c735a8
IndongLee <kundera@kakao.com>
2021-02-02 22:44:03 +0900

    Migration

---
+++ Jetpack/Compose/app/src/main/java/com/indong/compose/MigrationActivity.kt
@@ -0,0 +1,38 @@
+package com.indong.compose
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
+import com.indong.compose.ui.theme.ComposeTheme
+
+class MigrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting3("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting3(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview4() {
+    ComposeTheme {
+        Greeting3("Android")
+    }
+}
